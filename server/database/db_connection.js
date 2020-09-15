6f34f2a715461515dd53cd9e49fafa8c3cdb88c0
hoo00nn <hoo00nn@naver.com>
2020-09-15 22:34:43 +0900

    Feat: mysql2/promise 로 변경
    쿼리 결과를 순서대로 받기 위해 mysql2를 mysql2/promise로 변경

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const path = require('path');
 require('dotenv').config({ path : path.join(__dirname, '../.env') });
  
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-});
+})
 
-
+module.exports = {
+  pool
+}
