f763b18f4df33871bb673e430c8cd9b08ed98bf1
hoo00nn <hoo00nn@naver.com>
2020-09-16 02:00:57 +0900

    Feat: sideNav 메뉴 애니메이션 구현
    menu 버튼 클릭시 옆에서 슬라이드로 나타나게 구현

---
+++ client/src/javascripts/board.js
@@ -0,0 +1,11 @@
+(() => {
+  const board_header = document.querySelector('.board__header');
+
+  board_header.addEventListener('click', (e) => {
+    const classList = e.target.classList;
+    const sideNav = document.querySelector('.header__sidenav');
+    if (classList.contains('header__right') || classList.contains('sidenav__close-btn')) {
+      sideNav.classList.toggle('active');
+    }
+  })
+})();
