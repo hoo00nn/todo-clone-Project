8edf4f1628eec51532f2f8ed7fa89c8ef502a0c8
hoo00nn <hoo00nn@naver.com>
2020-09-16 02:00:14 +0900

    Feat: board 페이지 라우팅 작업
    board페이지 요청 시 랜더링 가능하도록 라우팅 작업

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const signup = require('./signup');
+const board = require('./board');
 
 router.use('/signup', signup);
+router.use('/board', board);
 
 router.get('/', function(req, res, next) {
   res.render('index');
