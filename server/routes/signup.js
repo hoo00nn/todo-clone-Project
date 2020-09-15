a8ef0df722c58010b3860742609544c2d359ff25
hoo00nn <hoo00nn@naver.com>
2020-09-15 21:09:00 +0900

    Feat: signup 페이지 라우팅 설정
    회원가입 페이지 요청시 랜더링 할 수 있도록 라우팅 연결 설정

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const signup = require('./signup');
-
-router.use('/signup', signup);
-
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('signup');
 });
 
 module.exports = router;
