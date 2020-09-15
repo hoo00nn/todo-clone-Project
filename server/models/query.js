36ef2b0911d36a2e9726c4c31fb13f2273ea6dda
hoo00nn <hoo00nn@naver.com>
2020-09-15 22:52:57 +0900

    Feat: 로그인 기능 구현
    사용자에게 입력받은 아이디, 비밀번호와 일치하는 컬럼이 있는지 DB에서 조회 후 있으면 로그인 성공, 없으면 로그인 실패

--- server/models/query.js
+++ server/models/query.js
@@ -1,4 +1,5 @@
 module.exports = {
   insertUser: `insert into user(id, password) values(?, ?)`,
-  findUser: `select * from user where id = ?`
+  findUser: `select * from user where id = ?`,
+  isCorrectUser: `select * from user where id = ? and password = ?`
 }
--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { USER } = require('../../models/user');
 
-router.post('/signin', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  const isCorrectUser = await USER.isCorrectUser(req.body.username, req.body.password);
+  if(isCorrectUser) return res.json({ status : 'success' });
+  return res.json({ status : 'fail'} );
 })
 
 module.exports = router;
