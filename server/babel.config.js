5ed3bf09fd430e1ec70815d139750bf4f1ee457e
hoo00nn <hoo00nn@naver.com>
2020-09-15 22:39:56 +0900

    Chore: 설정파일 필요없는 주석 제거
    바벨 설정파일에서 필요없는 주석 제거

--- server/babel.config.js
+++ server/babel.config.js
@@ -5,11 +5,6 @@ module.exports = {
       "targets": {
         "browsers": ["last 2 versions", "> 2% in KR"]
       },
-      // "useBuiltIns": "usage", // 폴리필 사용 방식 지정
-      //   "corejs": {
-      //     // 폴리필 버전 지정
-      //     "version": '3.6.5',
-      //   }
     }],
   ],
 }
