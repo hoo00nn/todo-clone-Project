329b975fba642ceea218cad9e7392c6afc193cd7
hoo00nn <hoo00nn@naver.com>
2020-09-19 00:05:46 +0900

    Feat: 로그인 여부 체크 로직 미들웨어로 추가
    로그인 여부에 따라 접근 권한이 달라지기 때문에 체크하는 미들웨어 추가 및 적용

--- server/routes/api/board.js
+++ server/routes/api/board.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const board = require('../../services/board');
+const { isLogined } = require('../../middleware/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', isLogined, async (req, res) => {
   const boardList = await board.getBoard();
   if (boardList) return res.status(200).json({ boardList });
   return res.status(400).json({ status : 'fail' });
--- server/routes/api/card.js
+++ server/routes/api/card.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const card = require('../../services/card');
+const { isLogined } = require('../../middleware/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', isLogined, async (req, res) => {
   const cardList = await card.getCardByID('test');
 
   if(cardList) return res.status(200).json({ cardList });
   return res.status(400).json({ status : 'fail' });
 });
 
-router.post('/', async (req, res) => {
+router.post('/', isLogined, async (req, res) => {
    const result = await card.insertCard(req.body);
 
    if (result) return res.status(201).json({ status : 'success' });
    return res.status(400).json({ status : 'fail' });
 });
 
-router.delete('/', async (req, res) => {
+router.delete('/', isLogined, async (req, res) => {
   const result = await card.deleteCard(req.body);
 
   if (result) return res.status(200).json({ status : 'success' });
   return res.status(400).json({ status : 'fail' });
 });
 
-router.patch('/', async (req, res) => {
+router.patch('/', isLogined, async (req, res) => {
   const result = await card.updateCard(req.body);
   
   if (result) return res.status(200).json({ status : 'success' });
