b9214543c20278dbfd4d618fc84e328816cf6519
hoo00nn <hoo00nn@naver.com>
2020-09-19 00:06:22 +0900

    Feat: 응답 메세지 관리하는 js 파일 생성
    응답 메세지를 따로 관리하려고 생성함

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../../services/user');
+const message = require('../../utils/response-message');
 
 router.post('/signin', async (req, res) => {
   const isCorrectUser = await user.signin(req.body.username, req.body.password);
   if(isCorrectUser) {
     req.session.username = req.body.username;
     req.session.save();
-    return res.status(200).json({ status : 'success' });
+    return res.status(200).json({ status : 'success', mssage : message.successToLogin });
   }
-  return res.status(400).json({ status : 'fail'} );
+  return res.status(400).json({ status : 'fail', message : message.failToLogin });
 })
 
 router.post('/signup', async (req, res) => {
   const isSignup = await user.signup(req.body.username, req.body.password);
 
-  if (isSignup) return res.status(200).json({ status : 'success' });
-  return res.status(400).json({ state : 'fail' });
+  if (isSignup) return res.status(200).json({ status : 'success', message : message.successToSignup });
+  return res.status(400).json({ state : 'fail', message : message.failToSignup });
+})
+
+router.post('/logout', async (req, res) => {
+  req.session.destroy();
+  res.status(201).json({ staus : 'success', message : message.successToLogout });
 })
 
 
---
+++ server/utils/response-message.js
@@ -0,0 +1,8 @@
+module.exports = {
+  requiredLogin: '로그인이 필요합니다.',
+  successToLogin: '로그인 성공',
+  failToLogin: '로그인 실패',
+  successToLogout: '로그아웃 성공',
+  failToSignup: '회원가입 실패',
+  successToSignup: '회원가입 성공'
+}
