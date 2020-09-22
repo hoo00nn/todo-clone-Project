de25f52c393aa8af8d1ffba398b631837b5d2190
hoo00nn <hoo00nn@naver.com>
2020-09-23 02:16:41 +0900

    Test: log api 테스트 코드 추가
    session이 유지되지 않아 임의로 사용자 정보를 입력하는 테스트 코드 추가

--- server/routes/api/log.js
+++ server/routes/api/log.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { logService } = require('../../services/log');
 const { isLogined } = require('../../middleware/auth');
 
-router.get('/', isLogined, async (req, res) => {
-  const logList = await logService.getLogByID(req.session.username);
+router.get('/', async (req, res) => {
+  // const logList = await logService.getLogByID(req.session.username);
+  const logList = await logService.getLogByID('test');
   if (logList) return res.status(200).json({ logList });
   return res.status(400).json({ status : 'fail' });
 });
